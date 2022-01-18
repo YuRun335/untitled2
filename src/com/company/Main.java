3893e213a1d9b2dba419439e31b857a390dbbb05
YuRun335 <bar72371@gmail.com>
2022-01-18 18:17:01 +0200

    -while-

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+
+
+
+
+    }
+
+    private void run() {
+        int[] mass = fillMass(20);
+        int max = mass[0];
+        int i = 1;
+        while (i < mass.length) {
+            if (mass[i] > max) max = mass[i];
+            i++;
+        }
+        System.out.println(max);
+    }
+
+    private int[] fillMass(int n) {
+        int[] mass = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            mass[i] = random.nextInt(100);
+        }
+        return mass;
+    }
+}
